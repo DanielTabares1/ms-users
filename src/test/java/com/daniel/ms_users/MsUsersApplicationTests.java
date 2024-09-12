94878c673915f5658d8a41db661de61db92ee4a2
Daniel Tabares <daniel.tabares1@udea.edu.co>
2024-09-12 10:44:09 -0500

    Init commit

---
+++ src/test/java/com/daniel/ms_users/MsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.ms_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
